1dfe7ee1538ffb677c84b03c645cd640ea24f0c5
MarkosComK <markoscomks@gmail.com>
2023-02-24 01:04:22 +0000

    calculator working

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -3,20 +3,92 @@ import { useState } from 'react'
 
 function Calculator(){
     // set first value
-    let [firstValue, setFirstValue] = useState('')
+    let [firstValue,  setFirstValue] = useState('')
+
     // set operation
     let [operation, setOperation] = useState('')
 
+    // set second value
+    let [secondValue, setSecondValue] = useState('')
+
+    let [result, setResult] = useState('')
+
     // function to push the values
     const addValue = (value: string) => {
-        setFirstValue(firstValue += value)
-        console.log(firstValue)
+        let maxLength = 5
+        if(operation.length === 0){
+            setFirstValue(firstValue += value)
+            if(firstValue.length > maxLength){
+                setFirstValue(firstValue = firstValue.slice(0, -1))
+            }
+        } else {
+            setSecondValue(secondValue += value)
+            if(secondValue.length > maxLength){
+                setSecondValue(secondValue = secondValue.slice(0, -1))
+            }
+        }
     }
 
     // function to set the operation
     const addOperation = (value: string) => {
-        setOperation(operation += value)
-        console.log(operation)
+        if(firstValue.length > 0){
+            setOperation(operation += value)
+            if(operation.length > 1){
+                setOperation(operation = operation.slice(0, -1))
+            }
+        }
+    }
+
+    // function to AC button
+    const clear = () => {
+        setFirstValue('')
+        setSecondValue('')
+        setOperation('')
+        setResult('')
+    }
+
+    // set second value percent
+    const percent = () => {
+        let percent
+        percent = Number(firstValue) / 100 * Number(secondValue)
+        setSecondValue(String(percent))
+    }
+
+    // function equal
+    const equal = () => {
+        let value
+        switch(operation){
+            case '÷':
+                value = Number(firstValue) / Number(secondValue)
+                setResult(result += String(value))
+                break
+            case '×':
+                value = Number(firstValue) * Number(secondValue)
+                setResult(result += String(value))
+                break
+            case '-':
+                value = Number(firstValue) - Number(secondValue)
+                setResult(result += String(value))
+                break
+            case '+':
+                value = Number(firstValue) + Number(secondValue)
+                setResult(result += String(value))
+                break
+        }
+        clear()
+        setFirstValue(firstValue = result)
+    }
+    // function to display the values of the calculator
+    const displayValue = () => {
+        // variablue used to replace . to ,
+        let value;
+        if (operation.length === 0) {
+          value = firstValue;
+        } else {
+          value = secondValue.length === 0 ? firstValue : secondValue;
+        }
+        value = value.replace('.', ',');
+        return value === '' ? '0' : value;
     }
 
     return(
@@ -29,18 +101,17 @@ function Calculator(){
                 </div>
             </S.Header>
             <S.Calculus>
-                {/* displays the value 0 at the start of calculator */}
-                <div>{firstValue == '' ? '0' : firstValue}</div>
+                <div>{displayValue()}</div>
                 <p>{operation}</p> 
             </S.Calculus>
             <S.Buttons>
-                <button id="clear" data-delete>AC</button>
-                <button onClick={() => {addOperation('％')}}>％</button>
+                <button onClick={clear}>AC</button>
+                <button onClick={percent}>％</button>
                 <button onClick={() => {addOperation('÷')}}>÷</button>
                 <button onClick={() => {addValue('7')}}>7</button>
                 <button onClick={() => {addValue('8')}}>8</button>
                 <button onClick={() => {addValue('9')}}>9</button>
-                <button id="mult" data-opt="x" >×</button>
+                <button onClick={() => {addOperation('×')}} >×</button>
                 <button onClick={() => {addValue('4')}}>4</button>
                 <button onClick={() => {addValue('5')}}>5</button>
                 <button onClick={() => {addValue('6')}}>6</button>
@@ -50,8 +121,8 @@ function Calculator(){
                 <button onClick={() => {addValue('3')}}>3</button>
                 <button onClick={() => {addOperation('+')}}>+</button>
                 <button onClick={() => {addValue('0')}}>0</button>
-                <button id="decimal" data-decimal=".">,</button>
-                <button id="equal" data-opt="=">=</button>
+                <button onClick={() => {addValue('.')}}>,</button>
+                <button onClick={equal}>=</button>
             </S.Buttons>
         </S.Calculator>
     )
