6fc848c38d6566a33950e2e0a4eacff6ac6bd4b7
MarkosComK <markoscomks@gmail.com>
2023-03-07 00:16:13 +0000

    movable div

--- src/components/MoveWindow/index.tsx
+++ src/components/MoveWindow/index.tsx
@@ -7,31 +7,54 @@ interface Props {
   children: JSX.Element
 }
 
+interface Position {
+    x: number,
+    y: number
+}
+
 function MoveWindow({ initialX, initialY, children }: Props) {
-  const [isDragging, setIsDragging] = useState(false)
-  const [position, setPosition] = useState({ x: initialX, y: initialY })
+    const [isDragging, setIsDragging] = useState(false)
+    const [position, setPosition] = useState<Position>({ x: initialX, y: initialY })
+    const mouseStart = useRef<Position>({ x: 0, y: 0 });
+    const mousePrev = useRef<Position>({ x: 0, y: 0 });
 
+    // activate the dragging
+    const handleDragOn = (e: React.MouseEvent) => {
+        setIsDragging(true)
+        mouseStart.current = { x: e.clientX, y: e.clientY }
+        mousePrev.current = mouseStart.current
+    }
+    // desactivate the dragging
+    const handleDragOff = (e: React.MouseEvent) => {
+        setIsDragging(false)
+    }
 
-  const handleMouseDown = () => {
-    setIsDragging(true)
-  }
+    const handleMouseMove = (e: React.MouseEvent) => {
+        if(isDragging){
+            const dx = e.clientX - mousePrev.current.x
+            const dy = e.clientY - mousePrev.current.y
+            setPosition((prevPosition) => ({
+                x: prevPosition.x + dx,
+                y: prevPosition.y + dy
+            }))
+            mousePrev.current = { x: e.clientX, y: e.clientY }
+        } 
+    }
 
-  const handleClick = () => {
-    console.log('click')
-  }
-
-  return (
-    <S.MovableWindow
-        top={position.x}
-        left={position.y}
-    >
-        <S.Header 
-        onMouseDown={handleMouseDown}
-        onClick={handleClick}
-        ></S.Header>
-        {children}
-    </S.MovableWindow>
-  );
-};
+    return (
+        <S.MovableWindow
+            top={position.y}
+            left={position.x}
+        >
+            <S.Header 
+                onMouseDown={handleDragOn}
+                onMouseUp={handleDragOff}
+                onMouseMove={handleMouseMove}
+            >
+            </S.Header>
+            {children}
+        </S.MovableWindow>
+    )
+}
 
 export default MoveWindow
